0e0f7f52f26baf04fe3852a42c2c6b166e36ba71
yinxf <yinxf@tuici.com>
2025-10-27 17:45:35 +0800

    init-2

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,65 +1,68 @@
 // src/App.tsx
-
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import cloudflareLogo from "./assets/Cloudflare_Logo.svg";
-import honoLogo from "./assets/hono.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [name, setName] = useState("unknown");
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://hono.dev/" target="_blank">
-          <img src={honoLogo} className="logo cloudflare" alt="Hono logo" />
-        </a>
-        <a href="https://workers.cloudflare.com/" target="_blank">
-          <img
-            src={cloudflareLogo}
-            className="logo cloudflare"
-            alt="Cloudflare logo"
-          />
-        </a>
+    <div class="w-full px-1 antialiased">
+      <div class="mx-auto max-w-screen-2xl px-4 sm:px-6 lg:px-8">
+        <main>
+          <div>
+            <div class="flex min-h-screen items-center justify-center">
+              <main class="flex flex-col items-center gap-8 sm:items-start">
+                <div
+                  class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+                  aria-hidden="true"
+                >
+                  <div
+                    class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+                    style="
+                                          clip-path: polygon(
+                                              74.1% 44.1%,
+                                              100% 61.6%,
+                                              97.5% 26.9%,
+                                              85.5% 0.1%,
+                                              80.7% 2%,
+                                              72.5% 32.5%,
+                                              60.2% 62.4%,
+                                              52.4% 68.1%,
+                                              47.5% 58.3%,
+                                              45.2% 34.5%,
+                                              27.5% 76.7%,
+                                              0.1% 64.9%,
+                                              17.9% 100%,
+                                              27.6% 76.8%,
+                                              76.1% 97.7%,
+                                              74.1% 44.1%
+                                          );
+                                      "
+                  ></div>
+                </div>
+                <div class="sm:mt-12 -mt-20 flex flex-col justify-center gap-10 md:flex-row md:items-center">
+                  <a href="https://elevva.ai">
+                    <img
+                      alt="elevva.ai"
+                      loading="lazy"
+                      width="307"
+                      height="45"
+                      decoding="async"
+                      data-nimg="1"
+                      class="-mt-1 mr-1 inline-block dark:invert"
+                      style="color: transparent"
+                      src="/elevva.png"
+                    />
+                  </a>
+                </div>
+                <div class="mt-40 flex w-full flex-col font-light tracking-widest text-black">
+                  <h4>
+                    <span class="font-bold">yin@elevva.ai</span>
+                  </h4>
+                </div>
+              </main>
+            </div>
+          </div>
+        </main>
       </div>
-      <h1>Vite + React + Hono + Cloudflare</h1>
-      <div className="card">
-        <button
-          onClick={() => setCount((count) => count + 1)}
-          aria-label="increment"
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <div className="card">
-        <button
-          onClick={() => {
-            fetch("/api/")
-              .then((res) => res.json() as Promise<{ name: string }>)
-              .then((data) => setName(data.name));
-          }}
-          aria-label="get name"
-        >
-          Name from API is: {name}
-        </button>
-        <p>
-          Edit <code>worker/index.ts</code> to change the name
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the logos to learn more</p>
-    </>
+    </div>
   );
 }
 
